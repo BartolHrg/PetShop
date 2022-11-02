b61cb43f5f4efc07da183ba55edaec8828f9e42e
BartolHrg <bartol.hrg.backup@gmail.com>
2022-11-02 14:15:38 +0100

    database works

--- pet/models.py
+++ pet/models.py
@@ -1,6 +1,5 @@
 from typing import *;
 
-from unicodedata import category;
 from django.db import models;
 try:
 	from django.db.models import BaseManager;
@@ -9,27 +8,37 @@ except ImportError:
 pass
 from django.http import QueryDict;
 
+__all__ = ["Category", "Tag", "Pet", "PhotoUrl", "queryListInCNF", "querryPetRequest", ];
+
 class Category(models.Model):
 	class Meta: app_label = "pet";
-	name: str = models.CharField();
+	name: str = models.CharField(max_length = 32);
 pass
 
 class Tag(models.Model):
 	class Meta: app_label = "pet";
-	name: str = models.CharField();
+	name: str = models.CharField(max_length = 32);
 pass
 
+statuses = ["available", "pending", "sold"];
+
 class Pet(models.Model):
-	class Meta: app_label = "pet";
-	name: str = models.CharField();
+	class Meta: 
+		app_label = "pet";
+		constraints = [
+			models.CheckConstraint(name="valid_status", check=models.Q(status__in=statuses)),
+		];
+	pass
+	name: str = models.CharField(max_length = 32);
 	category: Category = models.ForeignKey(Category, on_delete=models.CASCADE);
 	photo_urls: list[str];
-	tags: list[Tag] = models.ManyToManyField(Tag);
+	tags = models.ManyToManyField(Tag);
+	status: Literal["available"] | Literal["pending"] | Literal["sold"] = models.CharField(max_length = max(len(st) for st in statuses));
 pass
 
 class PhotoUrl(models.Model):
 	class Meta: app_label = "pet";
-	url: str = models.CharField();
+	url: str = models.CharField(max_length = 32);
 	pet: Pet = models.ForeignKey(Pet, on_delete=models.CASCADE);
 pass
 
@@ -77,3 +86,18 @@ def querryPetRequest(query: BaseManager[Pet], req_dict: QueryDict):
 	
 	return query;
 pass
+
+# if not list(Category.objects.all()):
+# 	cA = Category.objects.create(name="cA");
+# 	cB = Category.objects.create(name="cB");
+# 	cC = Category.objects.create(name="cC");
+# pass
+# if not list(Tag.objects.all()):
+# 	tA = Tag.objects.create(name="tA");
+# 	tB = Tag.objects.create(name="tB");
+# 	tC = Tag.objects.create(name="tC");
+# pass
+# if not list(Pet.objects.all()):
+# 	Pet.objects.create(name="pA", category_id=cB.id, tags=[tA, tC]);
+# 	Pet.objects.create(name="pB", category_id=cC.id, tags=[tB, tA]);
+# pass
--- pet/views.py
+++ pet/views.py
@@ -1,3 +1,4 @@
+import json
 from typing import *;
 from django.http import HttpRequest, HttpResponse;
 from django.shortcuts import render
@@ -7,7 +8,54 @@ from pet import models;
 
 class Pet(View):
 	def get(self, request: HttpRequest) -> HttpResponse:
-		return render(request, "./pet_list.html", {"greeting": "ja sam Bartol Hrg"});
+		pets = models.querryPetRequest(models.Pet.objects.all(), request.GET);
+		ptelsl = [pet for pet in pets];
+		for pet in ptelsl:
+			pet.tagsl = list(pet.tags.all());
+		pass
+		# print("\t\t", ptelsl);
+		return render(request, "./pet_list.html", {"pets": ptelsl});
+	pass
+	def post(self, request: HttpRequest) -> HttpResponse:
+		models.Category.objects.all().delete();
+		cA = models.Category.objects.create(name="cA");
+		cB = models.Category.objects.create(name="cB");
+		cC = models.Category.objects.create(name="cC");
+
+		models.Tag.objects.all().delete();
+		tA = models.Tag.objects.create(name="tA");
+		tB = models.Tag.objects.create(name="tB");
+		tC = models.Tag.objects.create(name="tC");
+		
+		models.Pet.objects.all().delete();
+		pA = models.Pet.objects.create(name="pA", category_id=cB.id, status="available"); pA.tags.set([tA, tC]);
+		pB = models.Pet.objects.create(name="pB", category_id=cC.id, status="sold"     ); pB.tags.set([tB, tA, tC]);
+		
+		# data = json.loads(request.body);
+		# # {
+		# # 	"name": "doggie",
+		# # 	"category": {
+		# # 		"id": 1,
+		# # # 		"name": "Dogs"
+		# # 	},
+		# # 	"photoUrls": [
+		# # 		"string"
+		# # 	],
+		# # 	"tags": [
+		# # 		{
+		# # 			"id": 0,
+		# # # 			"name": "string"
+		# # 		}
+		# # 	],
+		# # 	"status": "available"
+		# # }
+		# models.Pet.objects.create(
+		# 	name=data["name"],
+		# 	category=models.Category()
+		# )
+		...
+		# return HttpResponse(status=200);
+		return self.get(request);
 	pass
 pass
 
